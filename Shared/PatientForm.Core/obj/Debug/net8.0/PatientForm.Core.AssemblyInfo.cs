777cc3f7ccab70fa60ae69c990469061ec26df5d
RASEL <rasel@RASEL-PLACOVU>
2024-03-01 14:40:10 +0600

    fix cosmetic changes

--- Shared/PatientForm.Core/obj/Debug/net8.0/PatientForm.Core.AssemblyInfo.cs
+++ Shared/PatientForm.Core/obj/Debug/net8.0/PatientForm.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientForm.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb9ce37059f7a5a980fa399a2627ada81db6ffc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a439a2d8deb98a0175be2a8ca031bc8c6166f23a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientForm.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientForm.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
